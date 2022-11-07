adf4bef7158c0af0e34fe780667001e506f77bfa
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-11-07 07:52:39 -0300

    Adicionando exercicios

---
+++ 02-Exercicio/01-exercicio.js
@@ -0,0 +1,10 @@
+/**
+ * Crie um programa que dado um número imprima a sua tabuada
+ */
+
+const numero = 9;
+
+for (let i = 1; i <= 10; i++) {
+    let multiplicar = i * numero;
+    console.log(multiplicar);
+}
